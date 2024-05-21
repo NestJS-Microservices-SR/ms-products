a96783fc21a8bfa1e52d928afdbb2337ed29365d
soRodriguezz <s.rodriguezzapata01@gmail.com>
2024-05-21 12:18:49 -0400

    first commit

---
+++ src/products/products.controller.ts
@@ -0,0 +1,41 @@
+import { Controller, ParseIntPipe } from '@nestjs/common';
+import { MessagePattern, Payload } from '@nestjs/microservices';
+import { PaginationDto } from 'src/common/dto/pagination.dto';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { ProductsService } from './products.service';
+
+@Controller('products')
+export class ProductsController {
+  constructor(private readonly productsService: ProductsService) {}
+
+  @MessagePattern({ cmd: 'create_product' }) // nombre del patron
+  // con TCP no se envia un Body, se envia como Payload
+  create(@Payload() createProductDto: CreateProductDto) {
+    return this.productsService.create(createProductDto);
+  }
+
+  @MessagePattern({ cmd: 'find_all_products' })
+  findAll(@Payload() paginationDto: PaginationDto) {
+    return this.productsService.findAll(paginationDto);
+  }
+
+  @MessagePattern({ cmd: 'find_one_products' })
+  findOne(@Payload('id', ParseIntPipe) id: number) {
+    return this.productsService.findOne(id);
+  }
+
+  @MessagePattern({ cmd: 'update_product' })
+  update(
+    // @Param('id', ParseIntPipe) id: number,
+    // @Body() updateProductDto: UpdateProductDto,
+    @Payload() updateProductDto: UpdateProductDto,
+  ) {
+    return this.productsService.update(updateProductDto);
+  }
+
+  @MessagePattern({ cmd: 'delete_product' })
+  remove(@Payload('id', ParseIntPipe) id: number) {
+    return this.productsService.remove(id);
+  }
+}
---
+++ src/products/products.service.ts
@@ -0,0 +1,91 @@
+import {
+  Injectable,
+  Logger,
+  NotFoundException,
+  OnModuleInit,
+} from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { PrismaClient } from '@prisma/client';
+import { PaginationDto } from 'src/common/dto/pagination.dto';
+
+@Injectable()
+export class ProductsService extends PrismaClient implements OnModuleInit {
+  private readonly logger = new Logger('ProductsService');
+
+  async onModuleInit() {
+    await this.$connect();
+    this.logger.log('Database connected');
+  }
+
+  create(createProductDto: CreateProductDto) {
+    return this.product.create({ data: createProductDto });
+  }
+
+  async findAll(paginationDto: PaginationDto) {
+    const { page, limit } = paginationDto;
+
+    const totalRows = await this.product.count({ where: { available: true } });
+    const lastPage = Math.ceil(totalRows / limit);
+
+    if (page > lastPage) {
+      throw new NotFoundException('Page not found');
+    }
+
+    const data = await this.product.findMany({
+      take: limit,
+      skip: (page - 1) * limit,
+      where: {
+        available: true,
+      },
+    });
+
+    return {
+      metadata: {
+        page,
+        total: totalRows,
+        lastPage,
+      },
+      data,
+    };
+  }
+
+  async findOne(id: number) {
+    const product = await this.product.findFirst({
+      where: {
+        id,
+        available: true,
+      },
+    });
+
+    if (!product) {
+      throw new NotFoundException(`Product with id ${id} not found`);
+    }
+
+    return product;
+  }
+
+  async update(updateProductDto: UpdateProductDto) {
+    const { id, ...data } = updateProductDto;
+
+    await this.findOne(id);
+
+    return this.product.update({
+      where: { id },
+      data: data,
+    });
+  }
+
+  async remove(id: number) {
+    await this.findOne(id);
+    const product = await this.product.update({
+      where: { id },
+      data: { available: false },
+    });
+
+    return product;
+    // return this.product.delete({
+    //   where: { id },
+    // });
+  }
+}
