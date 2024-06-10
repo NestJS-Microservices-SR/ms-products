5480999a22734f10cbd95c3afbdbf757f3e937a6
soRodriguezz <s.rodriguezzapata01@gmail.com>
2024-06-10 17:48:45 -0400

    add controller validate product

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -93,4 +93,25 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
     //   where: { id },
     // });
   }
+
+  async validateProducts(ids: number[]) {
+    ids = Array.from(new Set(ids));
+
+    const products = await this.product.findMany({
+      where: {
+        id: {
+          in: ids,
+        },
+      },
+    });
+
+    if (products.length !== ids.length) {
+      throw new RpcException({
+        message: 'Some products were not found',
+        status: HttpStatus.BAD_REQUEST,
+      });
+    }
+
+    return products;
+  }
 }
